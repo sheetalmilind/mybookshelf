ae20911cc1f99ef9edfa8bb672c05577d6c017d3
sheetalmilind <87009373+sheetalmilind@users.noreply.github.com>
2025-01-23 21:04:06 +0530

    Delete bookshelf-backend directory

--- bookshelf-backend/routes/recommendations.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getRecommendations } = require('../controllers/recommendationsController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get('/', authMiddleware, getRecommendations);
-
-module.exports = router;
